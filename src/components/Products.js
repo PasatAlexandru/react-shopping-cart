7db55aff0fb64979bc1d42c140a9357514e7e8cd
PasatAlexandru <Palexandru8@icloud.com>
2020-11-23 03:10:52 +0200

    create product component

---
+++ src/components/Products.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import formatCurrency from "./utill";
+
+export class Products extends Component {
+    render() {
+        return (
+            <div>
+                <ul className="products">
+                    {this.props.products.map((product) => (
+                        <li key={product._id}>
+                            <div className="product">
+                                <a href={"#" + product._id}>
+                                    <img src={product.image} alt={product.title}/>
+                                    <p>{product.title}</p>
+                                </a>
+                                <div className="product-price">
+                                    <div style={{
+                                        display: "inline-block",
+                                        width:"50%"
+                                    }}>{formatCurrency(product.price)}</div>
+                                    <button
+                                        className="button primary"
+                                        style={{width:"50%"}}
+                                    >Add To Cart</button>
+                                </div>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default Products;
